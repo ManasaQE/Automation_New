3a474e4c12d3089fe1e1d755b0438fcdbd62e923
Tejas <Tejas@DESKTOP-4RD1FAI>
2022-12-26 22:03:12 +0530

    xpath examples

---
+++ AutomationTraining/src/com/account/com/XpathExamples.java
@@ -0,0 +1,29 @@
+package com.account.com;
+
+public class XpathExamples {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		//Xpath=//tagname[@attribute='value']
+		//Id,Name,Class Name,Link text,partial Link text,CSS selectors
+		// Xpath has been classified into 2 types:
+		//1. Absolute xpath
+		//2. Relative xpath
+		
+		//http://demo.guru99.com/test/selenium-xpath.html - link
+		
+		
+		//div[@class='featured-box cloumnsize1']//h4[1]//b[1] - relative xpath
+		//div[@class='featured-box']//h4[2]//b[1]
+		
+		//input[@name='uid']
+		
+		//img[@src='https://demo.guru99.com/images/java-logo.png']
+		
+		//*[contains(@name,'btn')]
+		
+	}
+
+}
