35fe7ed7596f50fb5755accc7bbed3cf658eee79
Tejas <Tejas@DESKTOP-4RD1FAI>
2022-12-27 21:56:26 +0530

    XPath examples

--- AutomationTraining/src/com/account/com/XpathExamples.java
+++ AutomationTraining/src/com/account/com/XpathExamples.java
@@ -23,6 +23,48 @@ public class XpathExamples {
 		
 		//*[contains(@name,'btn')]
 		
+		//*[contains(text(),'here')]
+		
+				//label[starts-with(@id,'message')]
+				
+				//td[text()='UserID']
+				
+				//Basic XPath- //tag name[@attribute name='value']
+				
+				//contains- 
+				
+				// XPath=//*[contains(@type,'sub')]   
+				// XPath=//*[contains(@id,'message')]
+				// XPath=//*[contains(text(),'here')]
+				// XPath=//*[contains(@href,'guru99.com')]
+				
+				//using or and and
+				// XPath=//*[@type='submit' or @name='btnReset']
+
+				// XPath Starts-with		
+				// XPath=//label[starts-with(@id,'message')]
+				
+				// XPath text function
+				// XPath=//td[text()='UserID']
+				
+				// XPath access methods are used to identify complex or dynamic elements.
+				/*
+				 * 1. Following 2. Ancestor 3. Child 4. Preceding 5. Following-sibling 6. Parent
+				 * 7. Self 8. Descendant
+				 */
+				
+				//*[text()='Live Projects']//ancestor::div
+				//*[@id='java_technologies']//child::li
+				
+				
+				/*
+				 * https://www.guru99.com/xpath-selenium.html - link to practice
+				 * 
+				 * Preceding Following-sibling: Parent: Self: Descendant:
+				 */
+			//	https://www.youtube.com/playlist?list=PLUzXmhjZTIVq5wWNg9QTr_Rk6hGfIUAy3 - github
+				
+		
 	}
 
 }
